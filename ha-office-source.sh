49542a49e148d8e3b5b3182fbaf598e562b06546
Jamie Smith <jamie@jamieandamy.com>
2019-07-07 09:11:06 -0400

    changed name of office sonos

--- ha-office-source.sh
+++ ha-office-source.sh
@@ -9,7 +9,7 @@ service="script.office_input_sonos"
 hacurlServices -s "${service}" -j "${json}"
 
 service="media_player/select_source"
-json='{"entity_id": "media_player.office", "source": '\"$choice\"'}'
+json='{"entity_id": "media_player.office_sonos", "source": '\"$choice\"'}'
 logIt $json
 hacurlServices -s "${service}" -j "${json}"
 
