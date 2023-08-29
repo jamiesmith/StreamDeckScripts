8877cacbae24f39b6be99f03514862a35e37d6b4
Jamie Smith <jamie@jamieandamy.com>
2023-08-29 13:22:34 -0400

    old stuff

--- j.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source $(pwd)/hacurl.sh
-
-hacurlStates -i states -e media_player.yamaha_receiver_office | /usr/local/bin/jq -r ".attributes.volume_level"
-hacurlStates -i states -e media_player.yamaha_receiver_office
