9d78ca2667af9c253027fe9fa1eb1b2d174fe7d4
Jamie Smith <github@jamieandamy.com>
2023-12-11 09:57:19 -0500

    fix addr for lights

--- elgato-keylight-air-toggle.sh
+++ elgato-keylight-air-toggle.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-status=$(curl --silent --location --request GET 'http://192.168.1.198:9123/elgato/lights' --header 'Accept: application/json' | /opt/homebrew/bin/jq '.lights[0].on')
+ADDR=http://192.168.1.220:9123
+status=$(curl --silent --location --request GET "${ADDR}/elgato/lights" --header 'Accept: application/json' | /opt/homebrew/bin/jq '.lights[0].on')
 echo status is $status
 if [ $status == 0 ]
 then
-    curl --location --request PUT 'http://192.168.1.198:9123/elgato/lights' --header 'Accept: application/json' --data-raw '{"numberOfLights": 1,"lights": [{"on": 1}]}'
+    curl --location --request PUT "${ADDR}/elgato/lights" --header 'Accept: application/json' --data-raw '{"numberOfLights": 1,"lights": [{"on": 1}]}'
 else
-    curl --location --request PUT 'http://192.168.1.198:9123/elgato/lights' --header 'Accept: application/json' --data-raw '{"numberOfLights": 1,"lights": [{"on": 0}]}'
+    curl --location --request PUT "${ADDR}/elgato/lights" --header 'Accept: application/json' --data-raw '{"numberOfLights": 1,"lights": [{"on": 0}]}'
 fi
 
 
